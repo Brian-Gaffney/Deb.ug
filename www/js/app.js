aec0a6665d3c1ec5e42880d0c96f27132b89df7a
Brian Gaffney <brian@deb.ug>
2012-10-19 15:13:17 +1100

    Lazy commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,18 @@
 app = {
 	window_focus: true,
 	init: function() {
+		$('body').addClass('window_focus');
 		curves.init();
 
-		var wall = new Masonry( document.getElementById('wrapper'));
+		var wall = new Masonry(document.getElementById('wrapper'));
 
 		//Detecing window focus/blur
 		function onBlur() {
+			$('body').removeClass('window_focus');
 			app.window_focus = false;
 		}
 		function onFocus(){
+			$('body').addClass('window_focus');
 			app.window_focus = true;
 		}
 		if (/*@cc_on!@*/false) { // check for Internet Explorer
