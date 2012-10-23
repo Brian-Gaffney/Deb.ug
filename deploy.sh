98577c93e864006fdbd5c0851db427368ac4d407
Brian Gaffney <brian@deb.ug>
2012-10-24 02:32:13 +1100

    Removed LESS. Fixed deployment. Changed JS loading.

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,26 @@ cp -r /var/www/deb.ug/www /tmp/deb.ug
 
 #JS compression and concatenation (uglifyjs2)
 echo 'Minifing and compressing JS files'
-cd /tmp/deb.ug/www/js/
-rm all.js less.js
-uglifyjs2 /tmp/deb.ug/www/js/* > /tmp/deb.ug/www/js/all.js
+#Parsing simple JSON using sed
+cd /tmp/deb.ug/www/
+rm js/all.js
+#Filthy hack because I can't get underscore-cli installed
+scripts=`cat js/scripts.json | sed -e 's/\[//g' -e 's/\]//g' -e 's/\"//g' -e 's/,//g' -e '/^$/d'`
+uglifyjs2 $scripts > /tmp/deb.ug/www/js/all.js
+
+# for script in $scripts
+# do
+# 	uglifyjs2 $script >> /tmp/deb.ug/www/js/all.js
+# done;
 
 #Remove original JS files
 echo 'Removing original JS files'
 cd /tmp/deb.ug/www/js/
 ls | grep -v 'all.js' | xargs rm
 
-#LESS compliation and CSS compression
+#CSS compression
 cd /tmp/deb.ug/www/
-lessc style.css -o style.less style.less
+yuicompressor style.css -o style.css
 
 #Find each file and gzip it
 echo 'Gzipping files'
@@ -32,7 +40,8 @@ do
 	mv $file'.gzip' $file
 done;
 
-echo 'Uploading...'
+echo 'Uploading to S3...'
+# Public turns on S3 website serving. CF-invalidate for Cloudfront cache, GZIP header for gzip data
 s3cmd  --acl-public --cf-invalidate --delete-removed --add-header=Content-Encoding:gzip sync /tmp/deb.ug/www/ s3://deb.ug
 
 echo 'Removing /tmp/deb.ug'
--- www/js/all.js
+++ www/js/all.js
@@ -1,10 +1,24 @@
-var scripts = ['js/less.js', 'js/jquery.min.js', 'js/jquery.masonry.min.js', 'js/raphael.min.js', 'js/curves.js', 'js/app.js'];
-var script;
+/***
+	This file is only used in development. It loads all the scripts specified in scripts.json
+	In production the scripts.json file is used to build a single concatenated js file
+****/
+
+ajaxRequest = new XMLHttpRequest();
+
+var scripts;
 var head= document.getElementsByTagName('head')[0];
 
-for (var i = 0; i < scripts.length; i++) {
-	script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = scripts[i];
-	head.appendChild(script);
-}
+ajaxRequest.onreadystatechange = function(){
+	if(ajaxRequest.readyState == 4){
+		scripts = JSON.parse(ajaxRequest.responseText);
+
+		for (var i = 0; i < scripts.length; i++) {
+			script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = scripts[i];
+			head.appendChild(script);
+		}
+	}
+};
+ajaxRequest.open("GET", "/js/scripts.json", true);
+ajaxRequest.send(null);
