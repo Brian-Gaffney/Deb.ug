dae988f9d7c0fbdc7f09be172ac211190226c144
Brian Gaffney <brian@deb.ug>
2013-02-28 00:48:26 +1100

    Lazy commit.

--- README.md
+++ README.md
@@ -4,12 +4,16 @@ Deb.ug
 Personal website
 
 Ideas:
+	Read more expands the box size and uses CSS3 transitions to do so smoothly
 	Have a grid of boxes with different things in each
 	Possible boxes:
-		Name, email etc.
 		Experience skills
 		JS demo
 		Anaconda
 
 Direct S3 path
 	http://deb.ug.s3-website-us-east-1.amazonaws.com/
+
+Bugs
+	At 75% zoom the scroll bar appears and disappears as the boxes fly around
+		Make the scroll bar always there.
--- www/js/app.js
+++ www/js/app.js
@@ -1,97 +1,128 @@
 app = {
 	window_focus: true,
+
 	init: function() {
+		app.ui.init();
+	},
 
-		//Masonry
-		$('#wrapper').masonry({
-			itemSelector : '.box',
-			colWidth: '250px'
-		});
+	ui: {
+		init: function() {
 
-		//Curves
-		curves.init();
-		$('.curves .loading').fadeOut();
-
-		//Availability
-		availability.init();
-
-		//Detecing window focus/blur
-		$('body').addClass('window_focus');
-		function onBlur() {
-			$('body').removeClass('window_focus');
-			app.window_focus = false;
-		}
-		function onFocus(){
-			$('body').addClass('window_focus');
-			app.window_focus = true;
-		}
-		if (/*@cc_on!@*/false) { // check for Internet Explorer
-			document.onfocusin = onFocus;
-			document.onfocusout = onBlur;
-		} else {
-			window.onfocus = onFocus;
-			window.onblur = onBlur;
-		}
-
-		/******
-		*** Bindings
-		********/
-		//Show more
-		$('.more-info-link').click(function(){
-			selector = $(this).data('more-selector');
-			item = $(selector).not(':visible'); //:visible is to make sure it's not already showing
-
-			var parent_box = $(this).parents('.box');
-			item.detach().hide().fadeIn().insertAfter(parent_box);
-
-			$('#wrapper').masonry('reload');
-		});
-
-		//Add Close buttons
-		var close_button = '<a href="javascript:;" class="close-button">✘</a>';
-		$('.box.more-info').each(function(i, item){
-			$(item).append(close_button);
-		});
-
-		//Close show more boxes
-		$('.more-info .close-button').click(function(){
-			var parent_box = $(this).parents('.box');
-
-			parent_box.detach().appendTo('#wrapper').fadeOut(null, function(){
-				$(this).appendTo('#templates');
+			//Masonry
+			$('#wrapper').masonry({
+				itemSelector : '.box',
+				colWidth: '250px'
 			});
 
-			$('#wrapper').masonry('reload');
-		});
+			//Curves
+			curves.init();
+			$('.curves .loading').fadeOut();
 
-		//Extendable boxes for showing more info
-		$('.box.curves').hover(function(){
-			$(this).find('.toggle-box-height').fadeIn();
-		}, function() {
-			$(this).find('.toggle-box-height').fadeOut();
-		});
+			//Availability
+			availability.init();
 
-		$('.toggle-box-height').click(function(){
-			t = $(this);
-			p = t.parent();
-
-			if( p.hasClass('double-high')) {
-				text = 'More information';
-				height = (p.height() / 2) + 'px';
+			//Detecing window focus/blur
+			$('body').addClass('window_focus');
+			function onBlur() {
+				$('body').removeClass('window_focus');
+				app.window_focus = false;
+			}
+			function onFocus(){
+				$('body').addClass('window_focus');
+				app.window_focus = true;
+			}
+			if (/*@cc_on!@*/false) { // check for Internet Explorer
+				document.onfocusin = onFocus;
+				document.onfocusout = onBlur;
 			} else {
-				text = 'Less information';
-				height = (p.height() * 2) + 'px';
+				window.onfocus = onFocus;
+				window.onblur = onBlur;
 			}
 
-			p.animate({
-				height: height
-			}, 500, function(){
-				p.toggleClass('double-high');
+			/******
+			*** Bindings
+			********/
+			//Show more
+			$('.more-info-link').click(function(){
+				selector = $(this).data('more-selector');
+				item = $(selector).not(':visible'); //:visible is to make sure it's not already showing
+
+				var parent_box = $(this).parents('.box');
+				item.detach().hide().fadeIn().insertAfter(parent_box);
+
+				$('#wrapper').masonry('reload');
 			});
 
-			t.text(text);
+			//Add expand and contract buttons to boxes that contain more info
+			var expand_contract_buttons = '<a href="javascript:;" class="expand-button">More ↘</a><a href="javascript:;" class="contract-button">Less ↖</a>';
+			$('.box.more-info').each(function(i, item){
+				$(item).append(expand_contract_buttons);
+			});
 
-		});
+			//Expand button for boxes
+			$('.box .expand-button, .box .contract-button').click(function(){
+
+				parent_box = $(this).parent('.box');
+
+				if(parent_box.hasClass('expanded')) {
+
+					parent_box
+						.removeClass('double-high double-wide expanded')
+						.css({
+							width: (parent_box.width() / 2) - parseInt(parent_box.css('margin-right'),10) / 2,
+							height: (parent_box.height() / 2) - parseInt(parent_box.css('margin-bottom'),10) /2
+						})
+					;
+
+				} else {
+					parent_box
+						.addClass('double-high double-wide expanded')
+						.css({
+							width: (parent_box.width() * 2) + parseInt(parent_box.css('margin-right'),10),
+							height: (parent_box.height() * 2) + parseInt(parent_box.css('margin-bottom'),10)
+						})
+						.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+							$('#wrapper').masonry('reload');	
+						})
+					;
+				}
+
+			});
+
+			//Extendable boxes for showing more info
+			$('.box.curves').hover(function(){
+				$(this).find('.toggle-box-height').fadeIn();
+			}, function() {
+				$(this).find('.toggle-box-height').fadeOut();
+			});
+
+			$('.toggle-box-height').click(function(){
+				t = $(this);
+				p = t.parent();
+
+				if( p.hasClass('double-high')) {
+					text = 'More information';
+					height = (p.height() / 2) + 'px';
+				} else {
+					text = 'Less information';
+					height = (p.height() * 2) + 'px';
+				}
+
+				p.animate({
+					height: height
+				}, 500, function(){
+					p.toggleClass('double-high');
+				});
+
+				t.text(text);
+
+			});
+		},
+
+		add_expand_button: function() {
+
+		}
+
 	}
 };
 
