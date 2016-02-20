4790d9a6a4cb83b08dd8207ba2223a8911484b4b
Brian Gaffney <brian@deb.ug>
2016-02-20 23:46:30 +1100

    Improved deployment process

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,20 @@
 #!/bin/sh
 echo '*** Deploying...'
 
-TMP_DIRECTORY=/tmp/deb.ug
-
-echo "Copying files to $TMP_DIRECTORY"
-rm -rf $TMP_DIRECTORY
-mkdir -p $TMP_DIRECTORY
-cp -r ./build/* $TMP_DIRECTORY
-
-
-cd $TMP_DIRECTORY/
-
-#Find each file and gzip it
-echo 'Gzipping files'
-files=`find $TMP_DIRECTORY/ -type f`
-for file in $files
-do
-	#Gzip em then rename them to their normal names
-	gzip -c $file > $file'.gzip'
-	echo $file'.gzip'
-	mv $file'.gzip' $file
-done;
+BUILD_DIRECTORY=./build
 
 echo 'Uploading to S3...'
-# Public turns on S3 website serving. CF-invalidate for Cloudfront cache, GZIP header for gzip data
-s3cmd  --acl-public --cf-invalidate --delete-removed --add-header=Content-Encoding:gzip sync $TMP_DIRECTORY/ s3://deb.ug
+s3cmd --acl-public sync $BUILD_DIRECTORY/ s3://deb.ug
 
-echo 'Removing $TMP_DIRECTORY'
-rm -rf $TMP_DIRECTORY
+# Create invalidation JSON for /index.html path
+echo "{\"Paths\":{\"Quantity\":1,\"Items\":[\"/index.html\"]},\"CallerReference\": \"deb.ug-deploy-`date +%s`\"}" > cf-invalidation.json
+
+# Invalidate www.deb.ug CloudFront distribution
+aws cloudfront create-invalidation --distribution-id EC9V66ZGO10ET --invalidation-batch file://cf-invalidation.json
+# And deb.ug CF distribution
+aws cloudfront create-invalidation --distribution-id EP4MIMELJ0BMS --invalidation-batch file://cf-invalidation.json
+
+# Delete invalidation file
+rm cf-invalidation.json
+
 echo '*** Done ***'
