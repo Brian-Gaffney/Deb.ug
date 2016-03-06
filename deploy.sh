eec83c705c3dc1282caa092e769b0e4e27fc33d7
Brian Gaffney <brian@deb.ug>
2016-03-06 17:34:55 +1100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,8 @@ s3cmd --acl-public sync $BUILD_DIRECTORY/ s3://deb.ug
 # Create invalidation JSON for /index.html path
 echo "{\"Paths\":{\"Quantity\":1,\"Items\":[\"/index.html\"]},\"CallerReference\": \"deb.ug-deploy-`date +%s`\"}" > cf-invalidation.json
 
-# Invalidate www.deb.ug CloudFront distribution
+# Invalidate CloudFront distribution
 aws cloudfront create-invalidation --distribution-id EC9V66ZGO10ET --invalidation-batch file://cf-invalidation.json
-# And deb.ug CF distribution
-aws cloudfront create-invalidation --distribution-id EP4MIMELJ0BMS --invalidation-batch file://cf-invalidation.json
 
 # Delete invalidation file
 rm cf-invalidation.json
