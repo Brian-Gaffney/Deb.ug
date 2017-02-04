9b8b155a2e00a60e5d2d7c1a9aa3591633234296
Brian Gaffney <brian@deb.ug>
2017-02-04 20:03:29 +1100

    Removed random colors stuff

--- src/colors.js
+++ src/colors.js
@@ -1,62 +1,32 @@
-import hexToRgb from 'hex-rgb';
-
-const colorVariations = [
-	{
-		primary: '#FFA500',
-		secondary: '#6666DD'
+export default {
+	text: {
+		r: 60,
+		g: 60,
+		b: 68,
+		rDecimal: 0.23529411764705882,
+		gDecimal: 0.23529411764705882,
+		bDecimal: 0.26666666666666666,
+		hex: "#3C3C44",
+		rgb: "60,60,68"
 	},
-	{
-		primary: '#6666DD',
-		secondary: '#FFA500'
+	primary: {
+		r: 255,
+		g: 165,
+		b: 0,
+		rDecimal: 1,
+		gDecimal: 0.6470588235294118,
+		bDecimal: 0,
+		hex: "#FFA500",
+		rgb: "255,165,0"
 	},
-	{
-		primary: '#22dddd',
-		secondary: '#CC4477'
-	},
-	{
-		primary: '#DD2222',
-		secondary: '#2222DD'
-	},
-	{
-		primary: '#2222DD',
-		secondary: '#DD2222'
+	secondary: {
+		r: 102,
+		g: 102,
+		b: 221,
+		rDecimal: 0.4,
+		gDecimal: 0.4,
+		bDecimal: 0.8666666666666667,
+		hex: "#6666DD",
+		rgb: "102,102,221"
 	}
-];
-
-// Get a random variation
-const variation = colorVariations[Math.floor(Math.random() * colorVariations.length)];
-
-// Merge defaults with random variation
-const colors = {
-	text: '#3C3C44',
-	...variation
-};
-
-const rgbHexColors = {};
-
-/*
- * Map the hex values to:
- * RGB string: eg. 220,110,50
- * Separate R, G and B ints 0-255
- * Separate decimal R G and B values from 0-1
- */
-Object.keys(colors).forEach((key, i) => {
-	const hex = colors[key];
-	const rgb = hexToRgb(hex);
-
-	// Individual RGB vals
-	const [r,g,b] = rgb;
-
-	rgbHexColors[key] = {
-		r,
-		g,
-		b,
-		rDecimal: r / 255,
-		gDecimal: g / 255,
-		bDecimal: b / 255,
-		hex,
-		rgb: rgb.join(',') // RGB string
-	};
-});
-
-export default rgbHexColors;
+}
